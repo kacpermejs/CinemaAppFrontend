8bdf07ab1ee374451e00c95fd38b72fbcd2499dc
kacpermejs <kacpermejs@gmail.com>
2023-05-02 22:21:13 +0200

    themes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CinemaAppFrontend';
+
+  private isDark = true;
+
+  @HostBinding('class')
+  get themeMode() {
+    return this.isDark ? 'theme-dark' : 'theme-light';
+  }
+
 }
